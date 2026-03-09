9519312b292265fa96c56ae796d8b2bd22959aed
julienb84 <julienbouchez@icloud.com>
2026-03-09 14:37:02 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// COMPONENTS //
+
+// REACT HOOKS & COMPONENTS //
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
