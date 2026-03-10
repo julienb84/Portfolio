46565445e358d2c4331664de258cdeed2645d355
julienb84 <julienbouchez@icloud.com>
2026-03-10 11:29:29 +0100

    setup CSS variables for tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,13 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <div className="flex justify-center text-app-content font-bold">
+        <h1>Joucou</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
