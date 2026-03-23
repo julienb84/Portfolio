521f59f06ed74d90673de2b95f6c4d6832a78bfd
julienb84 <julienbouchez@icloud.com>
2026-03-23 16:02:50 +0100

    add sitemap vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import autoprefixer from "autoprefixer";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    Sitemap({ hostname: "https://julienbouchez.com" }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
